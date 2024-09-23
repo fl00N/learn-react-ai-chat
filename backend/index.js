7b5e07f77f94bda0ec5e4417bba6bdedfea858a4
Andrii Bondar <andybondar3@gmail.com>
2024-09-23 13:00:51 +0100

    fix

--- backend/index.js
+++ backend/index.js
@@ -148,9 +148,6 @@ app.use((err, req, res, next) => {
   res.status(401).send("Unauthenticated!");
 });
 
-// PRODUCTION
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
 });
